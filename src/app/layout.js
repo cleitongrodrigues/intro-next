c518576129c9093f67a743b9ae3195c44d4d51d2
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-19 20:12:31 -0300

    aula 19/08/2024

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import Rodape from "@/componentes/rodape";
+import Header from "@/componentes/cabecalho";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-br">
       {/* <body className={inter.className}>{children}</body> */}
       <body>
+        <Header/>
         <div className="layout">
         {children}
         <Rodape/>
