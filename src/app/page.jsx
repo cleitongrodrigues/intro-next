025c12109310f8d46bf58d8e21bbdf1b7f07ea6e
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-08-26 20:28:12 -0300

    aula 26/08/2024

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,26 @@
-import Image from "next/image";
+import { MdLunchDining, MdLocalBar, MdDining, MdIcecream, MdFastfood } from "react-icons/md";
+
+import Slider from "@/componentes/slider";
+
+import ProdutosHome from "@/componentes/produtos/produtosHome";
 
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className="containerGlobal">
-        <div className={styles.containerTitle}>
-        <h1 className={styles.titulo}>Next</h1> 
-        <h2>Teste H2</h2>
-        <h3>Teste H3</h3>
-        <p>Teste paragrafo</p>
-        <div className={styles.containerImage}>
-          <Image
-          src={'/carro.jpg'} 
-          width={500}
-          height={500}
-          alt='carro'
-          className={styles.Image}/>
-        
-          <Image
-          src={'/carro.jpg'} 
-          width={100}
-          height={100}
-          alt='carro'
-          className={styles.Image}/>
+      <Slider/>
+
+        <div className={styles.tipos}>
+          <MdLunchDining className={styles.tpicon} />
+          <MdLocalBar className={styles.tpicon} />
+          <MdDining className={styles.tpicon} />
+          <MdIcecream className={styles.tpicon} />
+          <MdFastfood className={styles.tpicon} />
         </div>
+        
+        <ProdutosHome/>
+
       </div>
-    </div>
   );
 }
